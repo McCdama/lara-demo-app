2ac3aaa047236c38d02c26327c5992f7e9b9de40
Mohed Alrahbi <mccdama@gmail.com>
2026-02-25 13:07:57 +0100

    test - 01

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Testing Webhook
+|--------------------------------------------------------------------------
+|
+| test - 01
+|
+*/
